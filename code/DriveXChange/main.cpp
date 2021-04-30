2695021b75ec894a6c39284282f93a681ff9db35
Sven Arends <sven@unixguru.nl>
2021-04-30 23:49:25 +0200

    Started writing the new 'final' code, this time with comments to make clear what stuff does

--- code/DriveXChange/main.cpp
+++ code/DriveXChange/main.cpp
@@ -1,14 +1,16 @@
 #include "AnalogIn.h"
 #include "DigitalIn.h"
+#include "Motor.h"
 #include "PinNames.h"
+#include "Pixy2.h"
 #include "PwmOut.h"
+#include "VL53L1X.h"
 #include "mbed.h"
-
 #include "src/robot.h"
 
-//8 bit - 256
-//16 bit - 65535
-//32 bit - 4294967295
+// 8 bit - 256
+// 16 bit - 65535
+// 32 bit - 4294967295
 
 // =====-----------------------------------------------=====
 //
@@ -19,29 +21,29 @@ DigitalIn HW_SWITCH1(D2); // user defined switch on the PCB
 DigitalOut HW_LED1(LED1); // Led on PCB
 
 // PinName HW_MRS1(D6); // Motor 1 reset pin // D6
-PinName HW_MRS1(D2); // Motor 1 reset pin // D6
-PinName HW_MRS2(D7); // Motor 2 reset pin
-PinName HW_MRS3(D8); // Motor 3 reset pin (forklift)
+// PinName HW_MRS1(D2); // Motor 1 reset pin // D6
+// PinName HW_MRS2(D7); // Motor 2 reset pin
+// PinName HW_MRS3(D8); // Motor 3 reset pin (forklift)
 
 // PinName HW_M1IN1(D11); // Motor 1 control pin 1 // D11
 // PinName HW_M1IN2(D12); // Motor 1 control pin 2 // D12
-PinName HW_M1IN1(D4); // Motor 1 control pin 1 // D11
-PinName HW_M1IN2(D5); // Motor 1 control pin 2 // D12
+// PinName HW_M1IN1(D4); // Motor 1 control pin 1 // D11
+// PinName HW_M1IN2(D5); // Motor 1 control pin 2 // D12
 
-PinName HW_M2IN1(D9); // Motor 2 control pin 1
-PinName HW_M2IN2(D10); // Motor 2 control pin 2
+// PinName HW_M2IN1(D9);  // Motor 2 control pin 1
+// PinName HW_M2IN2(D10); // Motor 2 control pin 2
 
-PinName HW_M3IN1(A6); // Motor 3 control pin 1 (forklift)
-PinName HW_M3IN2(D3); // Motor 3 control pin 2 (forklift)
+// PinName HW_M3IN1(A6); // Motor 3 control pin 1 (forklift)
+// PinName HW_M3IN2(D3); // Motor 3 control pin 2 (forklift)
 
-PinName HW_LINE_DAT1(A0); // Line detection sensor 1
-PinName HW_LINE_DAT2(A1); // Line detection sensor 2
+// PinName HW_LINE_DAT1(A0); // Line detection sensor 1
+// PinName HW_LINE_DAT2(A1); // Line detection sensor 2
 
-PinName HW_CURRENTDETOUT1(A2); // Motor 1 current detection
-PinName HW_CURRENTDETOUT2(A3); // Motor 2 current detection
+// PinName HW_CURRENTDETOUT1(A2); // Motor 1 current detection
+// PinName HW_CURRENTDETOUT2(A3); // Motor 2 current detection
 
-PinName HW_HEFSW1(A4); // Forklift limit switch 1
-PinName HW_HEFSW2(A5); // Forklift limit switch 2
+// PinName HW_HEFSW1(A4); // Forklift limit switch 1
+// PinName HW_HEFSW2(A5); // Forklift limit switch 2
 
 // =====-----------------------------------------------=====
 //
@@ -55,7 +57,12 @@ PinName HW_HEFSW2(A5); // Forklift limit switch 2
 //
 // =====-----------------------------------------------=====
 ROBOT robot;
-Timer timer;
+Timer main_timer;
+Timer signal_timer;
+VL53L1X distance_sensor(I2C_SDA, I2C_SCL);
+Pixy2 pixy;
+Motor motor_left(A6, D7, D8);   // pwm, fwd, rev
+Motor motor_right(D3, D9, D10); // pwm, fwd, rev
 // =====-----------------------------------------------=====
 //
 //                   LIBRARY DEFINITION
@@ -76,48 +83,63 @@ void loop();
 // =====-----------------------------------------------=====
 
 int main() {
-    setup();
+  setup();
 
-    while (true) {
-        loop();
-    }
+  while (true) {
+    loop();
+  }
 }
 
 // Executed once on startup
 void setup() {
-    // Start timer and pass it to the robot logic
-    timer.start();
-    robot.setup_timer_instance(&timer);
+  // Start timer and pass it to the robot logic
+  main_timer.start();
+  signal_timer.start();
 
-    // Initialize the line detection sensor pins
-    robot.setup_pins_linedetection(HW_LINE_DAT1, HW_LINE_DAT2);
+  // Pass the timer instance to the robot code
+  robot.setup_timer_instance(&main_timer);
 
-    // Initialize the drive motors pins
-    robot.setup_pins_motor_left(HW_M1IN1, HW_M1IN2, HW_MRS1);
-    robot.setup_pins_motor_right(HW_M2IN1, HW_M2IN2, HW_MRS2);
+  // Initialize the distance sensor
+  robot.setup_distance_sensor(500); // read distance every 500ms
 
-    // initialize the drive motors current sensor pins
-    robot.setup_pins_motor_left_current_measurement(HW_CURRENTDETOUT1);
-    robot.setup_pins_motor_right_current_measurement(HW_CURRENTDETOUT2);
+  // Change the distance sensor offset
+  robot.set_distance_sensor_offset(85);
 
-    // Initialize the forklift motor & sensor pins
-    robot.setup_pins_motor_forklift(HW_M3IN1, HW_M3IN2, HW_MRS3);
-    robot.setup_pins_forklift_limit_switches(HW_HEFSW1, HW_HEFSW2); // limit_switch_up, limit_switch_down
+  // Initialize the line detection sensor pins
+  // robot.setup_pins_linedetection(HW_LINE_DAT1, HW_LINE_DAT2);
 
-    // Initialize the distance sensor
-    robot.setup_distance_sensor(50); // read distance every 500ms
+  // // Initialize the drive motors pins
+  // robot.setup_pins_motor_left(HW_M1IN1, HW_M1IN2);
+  // robot.setup_pins_motor_right(HW_M2IN1, HW_M2IN2);
 
-    // Change the distance sensor offset
-    robot.set_distance_sensor_offset(85);
+  // // initialize the drive motors current sensor pins
+  // robot.setup_pins_motor_left_current_measurement(HW_CURRENTDETOUT1);
+  // robot.setup_pins_motor_right_current_measurement(HW_CURRENTDETOUT2);
+
+  // // Initialize the forklift motor & sensor pins
+  // robot.setup_pins_motor_forklift(HW_M3IN1, HW_M3IN2);
+  // robot.setup_pins_forklift_limit_switches(HW_HEFSW1, HW_HEFSW2); //
+  // limit_switch_up, limit_switch_down
 }
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
 // Executed continuously
 void loop() {
-    // Blink onboard LED when the switch is off, otherwise run the robot code
-    // if (HW_SWITCH1 == 0) {
-    //     HW_LED1 = !HW_LED1;
-    //     ThisThread::sleep_for(500ms);
-    // } else {
-        robot.run();
-    // }
+  // Blink onboard LED when the switch is off, otherwise run the robot code
+  if (HW_SWITCH1 == 0) {
+    // HW_LED1 = !HW_LED1;
+    // ThisThread::sleep_for(500ms);
+    if (signal_timer.read_ms() % 500 == 0) {
+      HW_LED1 = !HW_LED1;
+    }
+  } else {
+    robot.run();
+    if (signal_timer.read_ms() % (HW_LED1 ? 25 : 250) == 0) {
+      HW_LED1 = !HW_LED1;
+    }
+  }
 }
+
+#pragma GCC diagnostic pop
--- code/MotorCode/main.cpp
+++ code/MotorCode/main.cpp
@@ -1,15 +1,190 @@
-#include "mbed.h"
-#include "VL53L1X.h"
-#include "Pixy2.h"
-// #include "I2C.h"
+// ------------------------------ //
+//            INCLUDES            //
+// ------------------------------ //
 
+#include "Motor.h"
+#include "Pixy2.h"
+#include "ThisThread.h"
+#include "VL53L1X.h"
+#include "mbed.h"
+
+// ------------------------------ //
+//           DEFINITIONS          //
+// ------------------------------ //
+
+// Pin definitions for I2C
 #define I2C_SDA D4
 #define I2C_SCL D5
 
-VL53L1X distance_sensor(I2C_SDA, I2C_SCL);
-Pixy2 line_camera;
+// Update interval of the Time-of-Flight(Vl53L1X) sensor. Time is in
+// milliseconds
+#define DISTANCE_SENSOR_UPDATE_INTERVAL_IN_MS 500
 
+// ------------------------------ //
+//             OBJECTS            //
+// ------------------------------ //
+
+// Distance tracking sensor to measure the distance in front of the robot, uses
+// the I2C protocol
+VL53L1X distance_sensor(I2C_SDA, I2C_SCL); // SDA(D4) SCL(D5)
+
+// The line tracking camera (Pixy2). Uses the SPI protocol (MOSI, MISO, SCK) for
+// communication. MOSI(D11) MISO(D12) SCK(D13)
+Pixy2 pixy_camera;
+
+// Motor on the left side of the motor (PWM FWD BCK)
+Motor motor_left(A6, D4, D5);
+
+// Motor on the right side of the motor (PWM FWD BCK)
+Motor motor_right(D3, D6, D7);
+
+// Set-up a default timer that runs since the robot has initialized
+Timer main_timer;
+
+// ------------------------------ //
+//            VARIABLES           //
+// ------------------------------ //
+
+// Automatically updated, the last time the distance sensor has updated is
+// stored in this variable in an unsigned 32 bit number. Time stored here is in
+// milliseconds and corresponds with DISTANCE_SENSOR_UPDATE_INTERVAL_IN_MS.
+uint32_t distance_sensor_read_last_milliseconds = 0;
+
+// The last measured distance is stored in this variable. Measured distance is
+// in millimeters. This variable is updated with an interval of
+// DISTANCE_SENSOR_UPDATE_INTERVAL_IN_MS milliseconds in the loop() function.
+int16_t distance_measured_in_mm = 0;
+
+// ------------------------------ //
+//            FUNCTIONS           //
+// ------------------------------ //
+
+// Dummy functions to be populated below, needed here because it needs to be
+// defined before 'int main() {'
+// setup is executed once when the code starts up
+void setup();
+// loop is executed continuesly after setup has finished
+void loop();
+
+// Executed when the microcontroller started, needs to have a while loop that
+// runs forever to prevent a dead lock that requires a power-cycle. This
+// function executes setup() once to initialize things like the pixy camera,
+// motor controllers, distance sensor, etc... and after that executes loop()
+// continously to let the robot function as it should. Like line following,
+// package delivery, distance tracking, etc...
 int main() {
+  setup();
+
   while (true) {
+    loop();
   }
 }
+
+// initialize things like the pixy camera,  motor controllers, distance sensor,
+// etc...
+void setup() {
+  // ---------- //
+  //  GENERAL   //
+  // ---------- //
+
+  // Wait a second before running the set-up code to make sure the Pixy2
+  // camera module has started and can be initialized. This prevents unnecessary
+  // timing issues between different microcontrollers (STM32 <=SPI=> Pixy2)
+  ThisThread::sleep_for(1000ms);
+
+  // Start the main timer
+  main_timer.start();
+
+  // ---------- //
+  //   PIXY2    //
+  // ---------- //
+
+  // Initialize the Pixy2 camera module. This allows the code to communicate to
+  // the Pixy2. Just a slap in the face to wake the Pixy2 module up and set-up
+  // the SPI interface to it.
+  pixy_camera.init();
+
+  // Change the pixy to run in line-tracking mode. This is an internal feature
+  // in the Pixy2 camera and gives the vector(s) back that can be used to keep
+  // track of the line.
+  pixy_camera.changeProg("line_tracking");
+
+  // Turn all light on the Pixy2 module on. setLamp(top, bottom), top are the
+  // two bright white LEDs to illuminate the road in front of it. bottom is the
+  // RGB LED on the bottom set as full white to help illuminate the road.
+  pixy_camera.setLamp(1, 1);
+
+  // Set the line features to onlt send the line vector(s) instead of line
+  // vector(s), barcodes and intersections. This saves up some time when
+  // communicating over SPI. To get all features instead of only the vector(s)
+  // set it to: (LINE_VECTOR | LINE_INTERSECTION | LINE_BARCODE).
+  pixy_camera.line.setMode(LINE_VECTOR);
+
+  // ---------- //
+  //  VL53L1X   //
+  // ---------- //
+
+  // Initialize the VL53L1X time-of-flight sensor and wake it up. Initializes
+  // the I2C communication to the sensor as well.
+  distance_sensor.begin();
+
+  // Set the sense modus. 2: Long range
+  distance_sensor.setDistanceMode(2);
+
+  // ---------- //
+  //   MOTORS   //
+  // ---------- //
+
+  // Turn the motors off on startup to make sure it doesn't drive when it
+  // shouldn't. Just a safety measure.
+  motor_left.speed(0.0);
+  motor_right.speed(0.0);
+}
+
+// Executed continously to let the robot function as it should. Like line
+// following, package delivery, distance tracking, etc...
+void loop() {
+  // ---------- //
+  //  GENERAL   //
+  // ---------- //
+
+  // Read the milliseconds since the startup of the microcontroller and store it
+  // in the local variable 'current_ms' accessable in the loop function. Stored
+  // in an unsigned 32 bit variable to prevent overflow. 32 bit unsigned integer
+  // has a max size of 4,294,967,295 which means that the microprocessor will
+  // overflow after about 49.7 days which is most plenty time to execute the
+  // required parcours. With an unsigned 16 bit number, which has a maximum size
+  // of 65535, it will overflow after about 10.9 minutes which would be enough
+  // but for safety an unsiged 32 bit number is used instead.
+  uint32_t current_ms =
+      chrono::duration_cast<chrono::milliseconds>(main_timer.elapsed_time())
+          .count();
+
+  // ---------- //
+  //  VL53L1X   //
+  // ---------- //
+
+  // Update the distance_measured_in_mm variable every
+  // DISTANCE_SENSOR_UPDATE_INTERVAL_IN_MS milliseconds since microcontroller
+  // startup. This is a non-block proces that uses the Mbed Timer module to
+  // update its variable. The timing is calculated by doing: current
+  // time(current_ms) - last time(distance_sensor_read_last_milliseconds) > max
+  // time(DISTANCE_SENSOR_UPDATE_INTERVAL_IN_MS). When that is true the last
+  // time(distance_sensor_read_last_milliseconds) variable is set to the current
+  // time(current_ms) and the cycle starts over.
+  if (current_ms - distance_sensor_read_last_milliseconds >
+      DISTANCE_SENSOR_UPDATE_INTERVAL_IN_MS) {
+    // Update the last milliseconds to the current milliseconds
+    distance_sensor_read_last_milliseconds = current_ms;
+    // Has the measurement completed and is the data ready to be read
+    if (distance_sensor.newDataReady()) {
+      // Update the distance variable for the robot to use
+      distance_measured_in_mm = distance_sensor.getDistance();
+    } else {
+      // Tell the VL53L1X sensor to start a measurement.
+      distance_sensor.startMeasurement();
+    }
+  }
+}
+
+// <<EOF>>
