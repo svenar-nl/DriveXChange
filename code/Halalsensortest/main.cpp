a533699016638c58520cded8b1ee73a8254416ac
BuildTools <unconfigured@null.spigotmc.org>
2021-05-21 14:30:38 +0200

    a

--- code/Halalsensortest/main.cpp
+++ code/Halalsensortest/main.cpp
@@ -1,20 +1,40 @@
 #include "mbed.h"
-
 // main() runs in its own thread in the OS
 
-InterruptIn halalIn(A2, PullDown);
+DigitalIn halalIn(A2, PullDown);
 
-void printRpm();
+Timer main_timer;
 
-int main()
-{
-    halalIn.rise(&printRpm);
+unsigned long pulseMillis = 0;
+int rpm = 0;
+bool pulseDetected = false;
+float distanceTraveled = 0;
+
+float WHEEL_DIAMETER = 0.15;
+
+void calculateRPM();
+
+int main() {
+    // halalIn.rise(&printRpm);
     while (true) {
-        
+        uint32_t current_ms = chrono::duration_cast<chrono::milliseconds>(main_timer.elapsed_time()).count();
+
+        if (!halalIn) {
+            if (!pulseDetected) {
+                pulseDetected = true;
+                calculateRPM();
+                pulseMillis = current_ms;
+            }
+        } else {
+            pulseDetected = false;
+        }
     }
 
 }
 
-void printRpm(){
-    printf("Toer");
+void calculateRPM() {
+    float distance = (float) WHEEL_DIAMETER * 3.1415;
+
+    distanceTraveled += distance;
+    printf("Distance: %.2fm", distanceTraveled);
 }
