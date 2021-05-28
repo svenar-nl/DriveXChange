4ec7f93080b9033b856ca81617358db626f495ac
BuildTools <unconfigured@null.spigotmc.org>
2021-05-28 14:19:07 +0200

    aaaaa

--- code/MotorCode/main.cpp
+++ code/MotorCode/main.cpp
@@ -284,7 +284,7 @@ void loop() {
     if (!halleffect_pulse_detected) {
       robot_distance_traveled += (float)WHEEL_DIAMETER * PI;
 
-    //   Deze toggle functie zorgt dat dit gedeelte van de functie niet meerdere keren per rondslag afstand optelt.
+      // Deze toggle functie zorgt dat dit gedeelte van de functie niet meerdere keren per rondslag afstand optelt.
       halleffect_pulse_detected = true;
     }
   } else {
